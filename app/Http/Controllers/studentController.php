00cc73ebde455c0658a55164ac057bfc8e76517b
arlansicat7878 <noemileinguevarra@gmail.com>
2026-05-28 15:53:56 +0800

    routes nalang kulang ko push ko maya

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\student;
 class studentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = student::all();
+        return view('students.index', compact('students'));
+
+    }
+    public function create()
+    {
+        return view('students.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'fname' => 'required',
+            'mname' => 'required',
+            'lname' => 'required',
+            'address' => 'required',
+            'date_of_birth' => 'required|date',
+            'contact_number' => 'required|date',
+        ]);
+
+        student::create($request->all());
+
+        return redirect()->route('students.index')
+                         ->with('success', 'Student created successfully.');
+    }
+
 }
